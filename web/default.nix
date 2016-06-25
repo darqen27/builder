183cbbcbf07634e59a3a3d14b8affa136929c257
Svein Ove Aas <sveina@gmail.com>
2016-06-25 15:37:21 +0100

    Adapted to Nix

---
+++ web/default.nix
@@ -0,0 +1,10 @@
+with import <nixpkgs> {};
+with stdenv;
+
+runCommand "erisia-website" {
+  src = builtins.filterSource
+    (path: type: type != "symlink")
+    ./.;
+  buildInputs = [ jekyll ];
+  shellHook = "jekyll server -d $(mktemp -d); exit";
+} "cd $src; jekyll build -d $out"
