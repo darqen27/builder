b5fffa3a8ef0aae38595b3a8ca2452a0289fffb3
Svein Ove Aas <sveina@gmail.com>
2016-08-18 23:14:28 +0100

    Harvestcraft. Yummy.

--- manifests/ModularMayhemExtras.nix
+++ manifests/ModularMayhemExtras.nix
@@ -1,5 +1,29 @@
 [
   {
+    name = "Pam_s_HarvestCraft_1.7.10Lb.jar";
+    filename = "Pam's HarvestCraft 1.7.10Lb.jar";
+    url = "https://minecraft.curseforge.com/projects/pams-harvestcraft/files/2270206/download";
+    md5 = "27115781353d0528f6f1c280e48c7ae1";
+  }
+  {
+    name = "AppleCore-mc1.7.10-1.3.2.jar";
+    filename = "AppleCore-mc1.7.10-1.3.2.jar";
+    url = "https://minecraft.curseforge.com/projects/applecore/files/2305594/download";
+    md5 = "d7b176f01a0e1492c9b9deff9e19d186";
+  }
+  {
+    name = "JAFFA_-_1.72.13";
+    filename = "JAFFA - 1.72.13.jar";
+    url = "https://minecraft.curseforge.com/projects/jaffa-foods-a-harvestcraft-addon/files/2283401/download";
+    md5 = "79d94660a75478521b722bf71890ac8e";
+  }
+  {
+    name = "SpiceOfLife-mc1.7.10-1.3.2.jar";
+    filename = "SpiceOfLife-mc1.7.10-1.3.2.jar";
+    url = "https://minecraft.curseforge.com/projects/the-spice-of-life/files/2305603/download";
+    md5 = "2b8416564ab0022dc18c49a4e1d7b2ce";
+  }
+  {
     name = "Smart_Moving_version_15.6_for_Minecraft_1.7.10";
     filename = "Smart Moving version 15.6 for Minecraft 1.7.10.jar";
     url = "https://minecraft.curseforge.com/projects/smart-moving/files/2248946/download";
