d4a74ac858dd6b2ae4239cb69d2c3a3b9a2377f5
Erisia administrator <minecraft@madoka.brage.info>
2016-11-24 20:40:24 +0000

    Split manifests by version

--- manifests/updateAll.sh
+++ manifests/updateAll.sh
@@ -5,8 +5,18 @@ set -eu
 cd $(dirname $0)
 
 for f in *.json; do
-    if [[ ${f:0:6} != 'fixed-' ]]; then
-       ./genNix.py < "$f" > $(basename "$f" .json).nix &
-    fi
+  if [[ "$f" == *"fixed"* ]]; then
+    continue
+  fi
+  if [[ "$f" == *"1.7.10"* ]]; then
+    # No 1.7 packs we'd like to update right now.
+    #./genNix.py 1.7.10 < "$f" > $(basename "$f" .json).nix &
+    true
+  elif [[ "$f" == *"1.10.2"* ]]; then
+    ./genNix.py 1.10.2 < "$f" > $(basename "$f" .json).nix &
+  else
+    #./genNix.py 1.7.10 < "$f" > 1.7.10-$(basename "$f" .json).nix &
+    ./genNix.py 1.10.2 < "$f" > 1.10.2-$(basename "$f" .json).nix &
+  fi
 done
 wait
