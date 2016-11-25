e4c947ecc52f62d87575301aa6c2d0686946b361
Erisia administrator <minecraft@madoka.brage.info>
2016-11-25 17:10:42 +0000

    Add controlling

--- manifests/1.10.2-base.nix
+++ manifests/1.10.2-base.nix
@@ -5,4 +5,10 @@
     url = "https://minecraft.curseforge.com/projects/thump/files/2340957/download";
     md5 = "7417cd54b78e895e33306a7198150934";
   }
+  {
+    name = "Controlling-1.0.0";
+    filename = "Controlling-1.0.0.jar";
+    url = "https://minecraft.curseforge.com/projects/controlling/files/2326858/download";
+    md5 = "c858c065c5cfa97fd6b2c3c8adbfcc16";
+  }
 ]
