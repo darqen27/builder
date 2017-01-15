b9571621293ea9e3b24103ee83c71fbcccba78e5
Erisia administrator <minecraft@madoka.brage.info>
2017-01-15 12:32:44 +0000

    Run CoFHtweaks client-side only

--- manifests/BR-Extras.nix
+++ manifests/BR-Extras.nix
@@ -46,6 +46,7 @@
     "encoded" = "CoFHTweaks-%5B1.7.10%5D1.1.2-89.jar";
     "projectPage" = "https://minecraft.curseforge.com/projects/cofhtweaks";
     "type" = "remote";
+    "side" = "client";
     "md5" = "cbb2193115b252c17cd7c03da9b8af3b";
   };
   "controlling" = {
