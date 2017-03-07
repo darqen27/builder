e6f59359b2720d118e7fcc5ddb50b0c087a460e7
Erisia administrator <minecraft@madoka.brage.info>
2017-03-07 14:47:18 +0000

    Add Bibliocraft

---
+++ manifest/Skyfactory-extras.nix
@@ -0,0 +1,20 @@
+{
+  "bibliocraft" = {
+    "src" = "https://minecraft.curseforge.com/projects/bibliocraft/files/2378365/download";
+    "version" = "2.2.1][MC1.10.2]";
+    "title" = "BiblioCraft";
+    "projectID" = 228027;
+    "filename" = "BiblioCraft[v2.2.1][MC1.10.2].jar";
+    "dependencies" = [];
+    "encoded" = "BiblioCraft%5Bv2.2.1%5D%5BMC1.10.2%5D.jar";
+    "projectPage" = "https://minecraft.curseforge.com/projects/bibliocraft";
+    "type" = "remote";
+    "md5" = "cb68229c96debd47041df78b3b991c93";
+  };
+  "bibliocraft-bibliowoods-natura-edition" = {
+    "projectID" = 228364;
+    "title" = "BiblioCraft: BiblioWoods Natura Edition";
+    "type" = "missing";
+    "projectPage" = "https://minecraft.curseforge.com/projects/bibliocraft-bibliowoods-natura-edition";
+  };
+}
