4d181967a26249f42c550df3edc287c1804a7922
Erisia administrator <minecraft@madoka.brage.info>
2017-05-11 19:59:56 +0100

    No more pokemon

--- manifest/Proton-extras.nix
+++ manifest/Proton-extras.nix
@@ -11,12 +11,4 @@
     "type" = "remote";
     "md5" = "46422c6dc6965b8823f535c173ec8c97";
   };
-  "pixelmon" = {
-    "src" = "https://madoka.brage.info/baughn/Pixelmon-1.10.2-5.0.4-universal.jar";
-    "filename" = "Pixelmon-1.10.2-5.0.4-universal.jar";
-    "version" = "1.10.2-5.0.4-universal";
-    "encoded" = "Pixelmon-1.10.2-5.0.4-universal.jar";
-    "type" = "remote";
-    "md5" = "da84fdf13ff4bb31611900a988b54066";
-  };
 }
