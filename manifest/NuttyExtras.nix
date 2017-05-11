7c7394bea8116516764c44ff4e1b48a3b8bf63b7
Erisia administrator <minecraft@madoka.brage.info>
2017-05-11 18:10:06 +0100

    NBT: Streams, Decocraft

---
+++ manifest/NuttyExtras.nix
@@ -0,0 +1,32 @@
+{
+  "decocraft" = {
+    "projectID" = 245375;
+    "title" = "decocraft";
+    "type" = "missing";
+    "projectPage" = "https://minecraft.curseforge.com/projects/decocraft";
+  };
+  "farseek" = {
+    "src" = "https://minecraft.curseforge.com/projects/farseek/files/2368930/download";
+    "version" = "1.9.4-2.1";
+    "title" = "Farseek";
+    "projectID" = 229708;
+    "filename" = "Farseek-1.9.4-2.1.jar";
+    "dependencies" = [];
+    "encoded" = "Farseek-1.9.4-2.1.jar";
+    "projectPage" = "https://minecraft.curseforge.com/projects/farseek";
+    "type" = "remote";
+    "md5" = "8a2840b92a9f9526ebc553eaf94f6ebb";
+  };
+  "streams" = {
+    "src" = "https://minecraft.curseforge.com/projects/streams/files/2349886/download";
+    "version" = "1.9.4-0.3";
+    "title" = "Streams";
+    "projectID" = 229769;
+    "filename" = "Streams-1.9.4-0.3.jar";
+    "dependencies" = [229708];
+    "encoded" = "Streams-1.9.4-0.3.jar";
+    "projectPage" = "https://minecraft.curseforge.com/projects/streams";
+    "type" = "remote";
+    "md5" = "cdc7c2a5ae1092ed0c4eeb7b906479dd";
+  };
+}
