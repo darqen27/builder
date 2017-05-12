fba8162569ab3a342350b2ea54ef332020a9fbc9
Erisia administrator <minecraft@madoka.brage.info>
2017-05-12 19:12:34 +0100

    Proton: Doggy Talents

--- manifest/Proton-extras.nix
+++ manifest/Proton-extras.nix
@@ -1,4 +1,16 @@
 {
+  "doggy-talents" = {
+    "src" = "https://minecraft.curseforge.com/projects/doggy-talents/files/2415078/download";
+    "version" = "1.10.2-v1.11.1d-universal";
+    "title" = "Doggy Talents";
+    "projectID" = 266567;
+    "filename" = "DoggyTalents-1.10.2-v1.11.1d-universal.jar";
+    "dependencies" = [];
+    "encoded" = "DoggyTalents-1.10.2-v1.11.1d-universal.jar";
+    "projectPage" = "https://minecraft.curseforge.com/projects/doggy-talents";
+    "type" = "remote";
+    "md5" = "78b09155600ddd55a979f3d1a2db752e";
+  };
   "earthworks" = {
     "src" = "https://minecraft.curseforge.com/projects/earthworks/files/2418292/download";
     "version" = "1.10.2-1.2.3";
