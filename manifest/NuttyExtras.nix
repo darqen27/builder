e66b9a17e449e6f77375e719aecdb11aaccc0508
Erisia administrator <minecraft@madoka.brage.info>
2017-05-13 15:23:35 +0100

    Add Bibliocraft

--- manifest/NuttyExtras.nix
+++ manifest/NuttyExtras.nix
@@ -1,4 +1,34 @@
 {
+  "bibliocraft" = {
+    "src" = "https://minecraft.curseforge.com/projects/bibliocraft/files/2378365/download";
+    "version" = "2.2.1][MC1.10.2]";
+    "title" = "BiblioCraft";
+    "projectID" = 228027;
+    "filename" = "BiblioCraft[v2.2.1][MC1.10.2].jar";
+    "dependencies" = [];
+    "encoded" = "BiblioCraft%5Bv2.2.1%5D%5BMC1.10.2%5D.jar";
+    "projectPage" = "https://minecraft.curseforge.com/projects/bibliocraft";
+    "type" = "remote";
+    "md5" = "cb68229c96debd47041df78b3b991c93";
+  };
+  "bibliocraft-bibliowoods-biomes-oplenty-edition" = {
+    "projectID" = 228362;
+    "title" = "BiblioCraft: BiblioWoods Biomes O&#x27;Plenty Edition";
+    "type" = "missing";
+    "projectPage" = "https://minecraft.curseforge.com/projects/bibliocraft-bibliowoods-biomes-oplenty-edition";
+  };
+  "bibliocraft-bibliowoods-forestry-edition" = {
+    "projectID" = 228363;
+    "title" = "BiblioCraft: BiblioWoods Forestry Edition";
+    "type" = "missing";
+    "projectPage" = "https://minecraft.curseforge.com/projects/bibliocraft-bibliowoods-forestry-edition";
+  };
+  "bibliocraft-bibliowoods-natura-edition" = {
+    "projectID" = 228364;
+    "title" = "BiblioCraft: BiblioWoods Natura Edition";
+    "type" = "missing";
+    "projectPage" = "https://minecraft.curseforge.com/projects/bibliocraft-bibliowoods-natura-edition";
+  };
   "decocraft" = {
     "projectID" = 245375;
     "title" = "decocraft";
